2cab7c0ebcbbdb84700c8d0fee087b462cc2d4de
Quoc <tranquoc12x@gmail.com>
2026-03-14 16:33:53 +0700

    format code

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ import cors from 'cors'; // 1. Import thêm cors
 import userRoutes from './src/routes/userRoutes.js';
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// 2. Kích hoạt middleware CORS cho phép frontend gọi tới
-// (Trong môi trường thực tế, bạn có thể truyền thêm origin của frontend vào đây để bảo mật hơn)
 app.use(cors()); 
-
 app.use(express.json());
 
-// 3. SỬA LỖI 404: Thêm tiền tố '/api' để khớp với đường dẫn bên frontend
+
+//API routes
 app.use('/users', userRoutes);
+
 
 // Server chạy backend
 app.listen(PORT, () => {
