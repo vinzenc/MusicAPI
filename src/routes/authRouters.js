f8f3705939b8dab224611be7210182d937452f8c
Quoc <tranquoc12x@gmail.com>
2026-04-08 13:24:00 +0700

    fix api reset password

--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login , forgotPassword, resetPassword} from "../controllers/authController.js";
 
 const router = express.Router();
 
